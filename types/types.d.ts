b1d463f4bb41a8ab22d24460644cb80ff132d3d5
CityHopper <peacestrokes@gmail.com>
2023-06-19 03:32:17 +0900

    Small changes

--- types/types.d.ts
+++ types/types.d.ts
@@ -3,27 +3,27 @@ import { ReactNode } from "react";
 export interface ICountry {
   altSpellings: string[];
   area: number;
+  borders: string[];
+  callingCodes: string[];
+  capital: string;
   cca2?: string;
   cca3?: string;
   cca4?: string;
+  cioc: string;
+  currencies: { code: string; name: string; symbol: string }[];
+  demonyms: { [key: string]: { f: string; m: string } };
+  flags: { alt: string; png: string; svg: string };
+  languages: { [key: string]: string };
   name: {
     common: string;
     official: string;
     nativeName: { [key: string]: { official: string; common: string } };
   };
   population: number;
-
-  capital: string;
   region: string;
   subregion: string;
   timezones: string[];
-  currencies: { code: string; name: string; symbol: string }[];
-  languages: { [key: string]: string };
-  flags: string[];
-  borders: string[];
   topLevelDomain: string[];
-  callingCodes: string[];
-  cioc: string;
 
   [key: string]: any;
 }
